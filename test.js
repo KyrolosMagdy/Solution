03651c19c6b68cc81997abbb36cd5ed64e90d1a4
unknown <pero.kero01@gmail.com>
2019-09-03 19:45:29 +0200

    the final solution

---
+++ test.js
@@ -0,0 +1,63 @@
+expo = '+ - * x 10 y 5';
+vara = {'x': 15, 'y': 3}
+
+function result(expression, variables){
+	const operators = [];
+	const nums = [];
+	var temporarly = [] ;
+	const expressionArray = expression.split(' ');
+	
+	expressionArray.map(ex => {
+		if(ex === '+' || ex === '-' || ex === '*' || ex ==='/'){
+			operators.push(ex);
+		} else if(isNaN(ex) === false){
+			nums.push(ex - '')
+		} else {
+			nums.push(variables[ex])
+		}
+	});
+	
+	function addToTemporarly(temporarly, num, operator) {
+		if(temporarly.length === 0){
+			operators.map(operator => {
+				if(operator === '+' && temporarly.length === 0){
+					temporarly.push(nums[0] + nums[1]);
+				}else if (operator === '-' && temporarly.length === 0){
+					temporarly.push(nums[0] - nums[1]);
+				}else if(operator === '*' && temporarly.length === 0){
+					temporarly.push(nums[0] * nums[1]);
+				}else if(operator === '/' && temporarly.length === 0){
+					temporarly.push(nums[0] / nums[1]);
+				}
+			})
+			nums.splice(0, 2);
+			operators.splice(0, 1);
+		}else if(temporarly.length > 0) {
+			operators.map(operator => {
+				if(operator === '+' && temporarly.length > 0){
+					temporarly.push( temporarly[temporarly.length-1] + nums[0]  );
+					nums.splice(0, 1);
+					operators.splice(0, 1);
+				}else if (operator === '-' && temporarly.length > 0){
+					temporarly.push( temporarly[temporarly.length-1] - nums[0]  );
+					nums.splice(0, 1);
+					operators.splice(0, 1);					
+				}else if(operator === '*' && temporarly.length > 0){
+					temporarly.push( temporarly[temporarly.length-1] * nums[0]);
+					nums.splice(0, 1);
+					operators.splice(0, 1);					
+				}else if(operator === '/' && temporarly.length > 0){
+					temporarly.push(  temporarly[temporarly.length-1] / nums[0] );
+					nums.splice(0, 1);
+					operators.splice(0, 1);					
+				}
+			})
+		}
+	}
+	while(nums.length > 0 && operators.length > 0){
+		addToTemporarly(temporarly, nums, operators);
+	}
+	console.log(nums, temporarly)
+	return temporarly[temporarly.length -1];
+}
+console.log(result(expo, vara))
